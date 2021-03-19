127f5e07594288a6cc2d301bc8f0bc252a5576b7
pohern <ohern_p@yahoo.com>
2021-03-19 09:50:28 -0500

    implement useContext to determine the amount of money spent so far

--- src/components/Spent.js
+++ src/components/Spent.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from '../context/AppContext'
 
 const Spent = () => {
+    const { expenses } = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost)
+    }, 0)
   return (
     <div className='alert alert-primary'>
-      <span>Spent so far: $2000</span>
+      <span>Spent so far: ${totalExpenses}</span>
     </div>
   );
 };
