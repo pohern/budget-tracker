a5a9513ecb5f40ac9b1b1e4cea9af35c47aa6c22
pohern <ohern_p@yahoo.com>
2021-03-19 09:38:06 -0500

    update onSubmit to pass payload to context

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext'
+import { v4 as uuidv4 } from 'uuid'
 
 const AddExpenseForm = () => {
+    const { dispatch } = useContext(AppContext)
+
+
     const [name, setName] = useState('')
     const [cost, setCost] = useState("");
 
     const onSubmit = (e) => {
         e.preventDefault()
-        alert('name ' + name + ' cost ' + cost)
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost)
+        }
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        })
     }
 
     return (
