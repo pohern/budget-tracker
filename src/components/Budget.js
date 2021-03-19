2d8776ad04db8fdd752692cfbab2c04fc82de71b
pohern <ohern_p@yahoo.com>
2021-03-19 11:05:14 -0500

    add feture to update budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext'
+import ViewBudget from './ViewBudget'
+import EditBudget from './EditBudget'
 
 const Budget = () => {
-    const { budget } = useContext(AppContext)
+    const { budget, dispatch } = useContext(AppContext)
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const handleEditClick = () => {
+        setIsEditing(true)
+    }
+
+    
+
     return ( 
-        <div className='alert alert-secondary'>
-            <span>Budget: ${ budget }</span>
+        <div className='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
+            {isEditing ? (
+                <EditBudget /> 
+            ) : (
+                <ViewBudget handleEditClick={handleEditClick} budget={budget}/>
+            )}
         </div>
      );
 }
---
+++ src/components/EditBudget.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditBudget = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default EditBudget;
