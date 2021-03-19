c4c129331348b11249a2a411fb79ddba7ab81277
pohern <ohern_p@yahoo.com>
2021-03-19 09:15:01 -0500

    import and implement use of context in App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Budget from './components/Budget'
-import Remaining from './components/Remaining'
-import Spent from './components/Spent'
-import ExpenseList from './components/ExpenseList';
-import AddExpenseForm from './components/AddExpenseForm';
-
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import Spent from "./components/Spent";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import { AppProvider } from "./context/AppContext";
 
 const App = () => {
   return (
-    <div className='container'>
-      <h1 className='mt-3'>My Budget Planner</h1>
-      <div className='row mt-3'>
-        <div className='col-sm'>
-          <Budget />
+    <AppProvider>
+      <div className='container'>
+        <h1 className='mt-3'>My Budget Planner</h1>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <Budget />
+          </div>
+          <div className='col-sm'>
+            <Remaining />
+          </div>
+          <div className='col-sm'>
+            <Spent />
+          </div>
         </div>
-        <div className='col-sm'>
-          <Remaining />
+        <h3 className='mt-3'>Expenses</h3>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <ExpenseList />
+          </div>
         </div>
-        <div className='col-sm'>
-          <Spent />
+        <h3 className='mt-3'>Add Expense</h3>
+        <div className='mt-3'>
+          <div className='col-sm'>
+            <AddExpenseForm />
+          </div>
         </div>
       </div>
-      <h3 className='mt-3'>Expenses</h3>
-      <div className='row mt-3'>
-        <div className='col-sm'>
-          <ExpenseList />
-        </div>
-      </div>
-      <h3 className='mt-3'>Add Expense</h3>
-      <div className='mt-3'>
-        <div className='col-sm'>
-          <AddExpenseForm />
-        </div>
-      </div>
-    </div>
+    </AppProvider>
   );
-}
- 
+};
+
 export default App;
