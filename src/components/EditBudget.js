5cd718649c3ec31c5abf295ab74b0885eab479de
pohern <ohern_p@yahoo.com>
2021-03-19 11:10:52 -0500

    update EditBudget component

--- src/components/EditBudget.js
+++ src/components/EditBudget.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const EditBudget = () => {
+const EditBudget = (props) => {
+    const [value, setValue] = useState(props.budget)
     return ( 
         <>
+        <input required type='number' className='form-control mr-3' id='name' value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type='button' className='btn btn-primary' onClick={(e) => props.handleSaveClick(value)}>
+            Save
+        </button>
         </>
      );
 }
