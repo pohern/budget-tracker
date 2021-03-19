d1b6626e1428b79f1147793c0cfdc8769a53a50b
pohern <ohern_p@yahoo.com>
2021-03-19 08:20:29 -0500

    add components fro budget, remaining, and spent

---
+++ src/components/Remaining.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Remaining = () => {
+    return (
+      <div className='alert alert-secondary'>
+        <span>Remaining: $2000</span>
+      </div>
+    );
+}
+ 
+export default Remaining;
