90f3e745dc27597f2c28dfd5ea4aa61eb53593ff
pohern <ohern_p@yahoo.com>
2021-03-19 09:26:22 -0500

    implement context on ExpenseList, and verify it is working as expected

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem'
+import { AppContext } from '../context/AppContext'
 
 const ExpenseList = () => {
-    const expenses = [
-        { id: 1, name: 'Shopping', cost: 50},
-        { id: 2, name: 'Groceries', cost: 150},
-        { id: 3, name: 'Gas', cost: 50},
-        { id: 4, name: 'Rent', cost: 520},
-        { id: 5, name: 'Presents', cost: 150},
-    ]
+    const { expenses } = useContext(AppContext)
+    // const expenses = [
+    //     { id: 1, name: 'Shopping', cost: 50},
+    //     { id: 2, name: 'Groceries', cost: 150},
+    //     { id: 3, name: 'Gas', cost: 50},
+    //     { id: 4, name: 'Rent', cost: 520},
+    //     { id: 5, name: 'Presents', cost: 150},
+    // ]
     return (
       <ul className='list-group'>
         {expenses.map((expense) => (
