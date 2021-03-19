edb4530cfc6c4da94c2c97179ea617814c55eace
pohern <ohern_p@yahoo.com>
2021-03-19 11:06:56 -0500

    add onClick for Editing

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -12,12 +12,18 @@ const Budget = () => {
         setIsEditing(true)
     }
 
-    
+    const handleSaveClick = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        })
+        setIsEditing(false)
+    }
 
     return ( 
         <div className='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
             {isEditing ? (
-                <EditBudget /> 
+                <EditBudget handleSaveClick={handleSaveClick} budget={budget}/> 
             ) : (
                 <ViewBudget handleEditClick={handleEditClick} budget={budget}/>
             )}
