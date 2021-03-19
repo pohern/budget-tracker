27e868ab73a5ef23119f349b75b35f24c5c69591
pohern <ohern_p@yahoo.com>
2021-03-19 10:46:19 -0500

    implemetn delete functionality

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TiDelete } from "react-icons/ti";
-
+import { AppContext } from '../context/AppContext'
 
 const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        })
+    }
+    
+
   return (
     <li className='list-group-item d-flex justify-content-between align-items-center'>
       {props.name}
@@ -10,7 +20,7 @@ const ExpenseItem = (props) => {
         <span className='badge badge-primary badge-pill mr-3'>
           ${props.cost}
         </span>
-        <TiDelete size='1.5em'></TiDelete>
+        <TiDelete onClick={handleDeleteExpense} size='1.5em'></TiDelete>
       </div>
     </li>
   );
