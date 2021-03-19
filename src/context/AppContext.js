9410151ca8e8951a11d91b06de3a3b862770c30c
pohern <ohern_p@yahoo.com>
2021-03-19 09:13:46 -0500

    set up context to use throughtout app

---
+++ src/context/AppContext.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from 'react'
+
+const AppReducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+const initalState = {
+  budget: 2000,
+  expenses: [
+    { id: 12, name: "shopping", cost: 40 },
+    { id: 13, name: "holiday", cost: 140 },
+  ],
+};
+
+export const AppContext = createContext()
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initalState)
+    return(<AppContext.Provider value={{
+        budget: state.budget,
+        expenses: state.expenses,
+        dispatch
+    }}>
+        {props.children}
+    </AppContext.Provider>)
+}
