05a1f1fd823f596c7b23df5f778737ce6ce7b648
pohern <ohern_p@yahoo.com>
2021-03-19 08:42:34 -0500

    add component to map over expenses, and verify that it is working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Budget from './components/Budget'
 import Remaining from './components/Remaining'
 import Spent from './components/Spent'
+import ExpenseList from './components/ExpenseList';
 
 
 
@@ -22,6 +23,11 @@ const App = () => {
         </div>
       </div>
       <h3 className='mt-3'>Expenses</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <ExpenseList />
+        </div>
+      </div>
     </div>
   );
 }
